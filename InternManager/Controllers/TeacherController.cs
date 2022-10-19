ede8653c14ff2919a580f0f7a99ae6ec0d6d2728
Caner Ay Celep <72454642+caner24@users.noreply.github.com>
2022-10-19 12:57:00 +0300

    Added excell to db views and controllers.

--- InternManager/Controllers/TeacherController.cs
+++ InternManager/Controllers/TeacherController.cs
@@ -112,9 +112,83 @@ namespace InternManager.WebUI.Controllers
 
       
         [HttpGet]
-        public IActionResult ExcellList(IFormCollection student)
+        public IActionResult ExcellToDb(IFormCollection student)
         {
             return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult ExcellToDb(IFormFile postedFile)
+        {
+            if (postedFile != null)
+            {
+                //Create a Folder.
+                string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                //Save the uploaded Excel file.
+                string fileName = Path.GetFileName(postedFile.FileName);
+                string filePath = Path.Combine(path, fileName);
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                //Read the connection string for the Excel file.
+                string conString = this.Configuration.GetConnectionString("ExcelConString");
+                DataTable dt = new DataTable();
+                conString = string.Format(conString, filePath);
+
+                using (OleDbConnection connExcel = new OleDbConnection(conString))
+                {
+                    using (OleDbCommand cmdExcel = new OleDbCommand())
+                    {
+                        using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                        {
+                            cmdExcel.Connection = connExcel;
+
+                            //Get the name of First Sheet.
+                            connExcel.Open();
+                            DataTable dtExcelSchema;
+                            dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                            string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                            connExcel.Close();
+
+                            //Read Data from First Sheet.
+                            connExcel.Open();
+                            cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                            odaExcel.SelectCommand = cmdExcel;
+                            odaExcel.Fill(dt);
+                            connExcel.Close();
+                        }
+                    }
+                }
+
+                //Insert the Data read from the Excel file to Database Table.
+                conString = this.Configuration.GetConnectionString("constr");
+                using (SqlConnection con = new SqlConnection(conString))
+                {
+                    using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                    {
+                        //Set the database table name.
+                        sqlBulkCopy.DestinationTableName = "dbo.Persons";
+
+                        //[OPTIONAL]: Map the Excel columns with that of the database table
+                        sqlBulkCopy.ColumnMappings.Add("IdentyNumber", "IdentyNumber");
+                        sqlBulkCopy.ColumnMappings.Add("NameSurname", "NameSurname");
+                        sqlBulkCopy.ColumnMappings.Add("Gender", "Gender");
+
+                        con.Open();
+                        sqlBulkCopy.WriteToServer(dt);
+                        con.Close();
+                    }
+                }
+            }
+
+            return View();
         }
 
     }
