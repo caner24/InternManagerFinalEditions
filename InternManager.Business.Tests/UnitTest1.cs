0c858dffbe8837732af83d95feb762f20424df80
Caner Ay Celep <72454642+caner24@users.noreply.github.com>
2022-10-19 13:02:27 +0300

    Change some tests file

--- InternManager.Business.Tests/UnitTest1.cs
+++ InternManager.Business.Tests/UnitTest1.cs
@@ -9,16 +9,17 @@ namespace InternManager.Business.Tests
         public void Setup()
         {
         }
+
         [Test]
         public void Test1()
         {
             SqlConnection conn = new SqlConnection("Server=tcp:internmanager.database.windows.net,1433;Initial Catalog=InternManager;Persist Security Info=False;User ID=myAdmin;Password=123456-Admin;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"); // assert
             conn.Open();
-            SqlCommand komut = new SqlCommand("Select * from Persons",conn); 
+            SqlCommand komut = new SqlCommand("Select * from Persons", conn);
 
 
-            string id="";
-            SqlDataReader dr = komut.ExecuteReader();   
+            string id = "";
+            SqlDataReader dr = komut.ExecuteReader();
             while (dr.Read())
             {
                 id = dr["Id"].ToString();                     // act
